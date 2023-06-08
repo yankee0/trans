d0145053c90c974f7fe93a7a87e18f41a320d340
yankee <yankeesuprem@gmail.com>
2023-06-08 16:38:30 +0000

    migration ligne de facture

---
+++ app/Database/Migrations/2023-06-08-162956_FactLivLignes.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class FactLivLignes extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_lieu' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'null' => true,
+            ],
+            'designation' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'conteneur' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'type' => [
+                'type' => 'ENUM("20","40")',
+                'null' => true,
+            ],
+            'prix' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('id_lieu','fact_liv_lieux','id','CASCADE','CASCADE');
+        $this->forge->createTable('fact_liv_lignes',true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('fact_liv_lignes',true);
+    }
+}
