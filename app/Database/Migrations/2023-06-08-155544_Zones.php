870a22a7481119dd9480ecf50c22e30316d0ca1c
yankee <yankeesuprem@gmail.com>
2023-06-08 16:00:30 +0000

    migration zones

---
+++ app/Database/Migrations/2023-06-08-155544_Zones.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Zones extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'tva' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true,
+                'default' => 0
+            ],
+            'ht_20' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'default' => 0
+            ],
+            'ht_40' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true,
+                'default' => 0
+            ],
+            'carburant' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true,
+                'default' => 0
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('zones',true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('zones',true);
+    }
+}
