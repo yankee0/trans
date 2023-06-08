b98cf5885f7d84b9d4c07ba9feb4c8b45f24d5f7
yankee <yankeesuprem@gmail.com>
2023-06-08 08:07:59 +0000

    migration utilisateurs

---
+++ app/Database/Migrations/2023-06-08-075454_Utilisateurs.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Utilisateurs extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'profil' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'tel' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'mdp' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addUniqueKey('tel');
+        $this->forge->addUniqueKey('email');
+        $this->forge->createTable('utilisateurs',true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('utilisateurs',true);
+    }
+}
