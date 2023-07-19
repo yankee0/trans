19cae8200ff77c928ee7b36b9e1fd4d3d8cdeb59
yankee <yankeesuprem@gmail.com>
2023-07-19 18:22:41 +0000

    migration

---
+++ app/Database/Migrations/2023-07-19-181823_TvaAgsCopie.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TvaAgsCopie extends Migration
+{
+    public function up()
+    {
+        $this->forge->addColumn('fact_liv',[
+            'avec_tva' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'default' => 'NON'
+            ],
+            'avec_ages' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'default' => 'OUI'
+            ],
+            'avec_copie' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+                'default' => 'NON'
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        $this->forge->dropColumn('fact_liv','avec_ags');
+        $this->forge->dropColumn('fact_liv','avec_copie');
+        $this->forge->dropColumn('fact_liv','avec_tva');
+    }
+}
