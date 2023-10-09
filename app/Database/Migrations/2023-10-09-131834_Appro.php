6c35ef9e8d2807829b8d29d916b6b4725fcaf664
yankee <yankeesuprem@gmail.com>
2023-10-09 13:23:29 +0000

    migration appro

---
+++ app/Database/Migrations/2023-10-09-131834_Appro.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Appro extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'date' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'nature' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'auteur' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'montant' => [
+                'type' => 'DOUBLE',
+                'null' => true,
+                'default' => 0
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('appro');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('appro');
+    }
+}
