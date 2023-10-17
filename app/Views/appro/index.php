2baca5b870a82b54ec3539b7e8a2bafece8062fe
yankee <yankeesuprem@gmail.com>
2023-10-17 14:42:30 +0000

    correction

--- app/Views/appro/index.php
+++ app/Views/appro/index.php
@@ -304,14 +304,14 @@ Approvisionnements
 
 <!-- show img -->
 <div class="modal fade" id="modalIdshowImg" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleIdImgShow" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md" role="document">
+  <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="modalTitleIdImgShow">Apperçu</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <img src="" height="500px" class="margin-auto" id="imgField">
+        <img src="" class="margin-auto w-100" id="imgField">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
